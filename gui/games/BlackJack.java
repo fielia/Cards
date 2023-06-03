c326a98d1c37f492511a56a14e4766a5ea61ff3f
Rit Garg <100240647+fielia@users.noreply.github.com>
2023-06-02 22:36:50 -0400

    copying text game files to gui, as a base

---
+++ gui/games/BlackJack.java
@@ -0,0 +1,181 @@
+package gui.games;
+
+import gui.backend.*;
+
+import java.util.Scanner;
+
+import java.awt.LayoutManager;
+
+public class BlackJack extends BaseGame {
+	
+	public int cost() {
+		return 10;
+	}
+	
+	public String rules() {
+		return "The goal of this BaseGame is to get the total sum of your cards to be greater than the Dealer's, while " +
+				       "not going over 21. Aces are worth 1 point in this version, number cards are worth their rank " +
+				       "value, and Jacks, Queens, and Kings are worth 10 points. If you 'bust', or go over 21 points," +
+				       " the Dealer wins one point, and vice versa. If neither goes over 21, whoever gets the larger " +
+				       "amount of points in their hand wins, or both win a point if it's a tie.";
+	}
+	
+	/**
+	 * point value for the player
+	 */
+	private int pointA = 0;
+	/**
+	 * point value for the computer
+	 */
+	private int pointB = 0;
+	
+	public String toString() {
+		return "BlackJack (version 1)";
+	}
+	
+	protected int playerLimit() {
+		return 1;
+	}
+
+	public BlackJack(String title, int width, int height, LayoutManager layout) {
+		super(title, width, height, layout);
+	}
+	
+	/**
+	 * checks how many overall points the player and computer have
+	 *
+	 * @throws InterruptedException Thread.sleep() throws this
+	 */
+	private int scoreCheck() throws InterruptedException {
+		String suffix = "";
+		int outcome;
+		if (Math.abs(pointA - pointB) != 1) {
+			suffix = "s";
+		}
+		Thread.sleep(1000);
+		if (pointB > pointA) {
+			System.out.println("Dealer wins by " + (pointB - pointA) + " point" + suffix + "! " +
+					                   "Maybe next time you'll win!");
+			outcome = 0;
+		} else if (pointA > pointB) {
+			System.out.println(BaseGame.getName(0) + ", you win by " + (pointA - pointB) + " point" + suffix + "! " +
+					                   "Congratulations!");
+			outcome = 2;
+		} else {
+			System.out.println("It's a tie!");
+			outcome = 1;
+		}
+		Thread.sleep(2000);
+		pointA = 0;
+		pointB = 0;
+		return outcome;
+	}
+	
+	/**
+	 * @param hand list of cards to calculate value from
+	 * @return value of hand parameter
+	 */
+	private int handValue(Hand hand) {
+		int value = 0;
+		for (Card card : hand) {
+			value += card.getRankValue();
+		}
+		return value;
+	}
+	
+	public int play() throws InterruptedException {
+		Scanner scanner = new Scanner(System.in);
+		char again = 'y';
+		while (again == 'y') {
+			System.out.println("\n----------------------------\n\nLet's play Blackjack!\n");
+			Deck deck = new Deck();
+			deck.modifyRanks(new String[] {"Ace", "King", "Queen", "Jack"}, new int[] {1, 10, 10, 10});
+			Hand handA = new Hand(2, deck, 2);
+			Hand handB = new Hand(2, deck, 2);
+			int valueA;
+			int valueB;
+			scanner = new Scanner(System.in);
+			System.out.println("Your hand:");
+			handA.printHand();
+			valueA = handValue(handA);
+			System.out.println("Your hand's value: " + valueA);
+			Thread.sleep(1000);
+			System.out.println("\nDealer's card: " + handB.get(0));
+			String choice = "";
+			while (valueA <= 21) {
+				System.out.print("Do you want to hit or stay? ");
+				choice = scanner.nextLine().toLowerCase();
+				if (choice.equals("hit")) {
+					handA.add(deck.draw());
+					System.out.println("\nYour hand:");
+					handA.printHand();
+					valueA = handValue(handA);
+					System.out.println("Your hand's value: " + valueA);
+				} else if (choice.equals("stay")) {
+					break;
+				}
+				if (!choice.equals("hit")) {
+					System.out.println("Please only enter 'hit' or 'stay'.");
+				}
+			}
+			if (choice.equals("hit")) {
+				System.out.println("You're Busted");
+				pointB++;
+				Thread.sleep(2000);
+			} else {
+				System.out.println("\nDealer's turn.");
+				valueB = 0;
+				while (valueB < 17) {
+					Thread.sleep(3000);
+					System.out.println("\nDealer's hand:");
+					Thread.sleep(2000);
+					handB.printHand();
+					valueB = handValue(handB);
+					Thread.sleep(2000);
+					System.out.println("Dealer's hand value: " + valueB);
+					handB.add(deck.draw());
+					Thread.sleep(3000);
+					if (valueB < 17) {
+						System.out.println("The Dealer decides to hit.");
+					} else if (valueB <= 20) {
+						System.out.println("The Dealer decides to stay.");
+					}
+				}
+				if (valueB > 20) {
+					System.out.println("Dealer busted.");
+					pointA++;
+					Thread.sleep(2000);
+				} else if (valueB > valueA) {
+					Thread.sleep(1000);
+					System.out.println("Dealer wins! Better luck next time!");
+					pointB++;
+					Thread.sleep(2000);
+				} else if (valueB < valueA) {
+					Thread.sleep(1000);
+					System.out.println("You win, " + BaseGame.getName(0) + "! Congratulations!");
+					pointA++;
+					Thread.sleep(2000);
+				} else {
+					Thread.sleep(1000);
+					System.out.println("It's a tie! You both get a 'win point'.");
+					pointA++;
+					pointB++;
+					Thread.sleep(2000);
+				}
+			}
+			System.out.println("\nThe current score is You: " + pointA + ", and Dealer: " + pointB + ".");
+			do {
+				if (again != 'y') {
+					System.out.println("You did not type 'y' or 'n'. Please choose one.");
+				}
+				Thread.sleep(2000);
+				System.out.println("Would you like to play again, " + BaseGame.getName(0) + "?");
+				again = scanner.next().toLowerCase().charAt(0);
+			}
+			while (again != 'n' && again != 'y');
+		}
+		System.out.println("OK. Hope to see you soon!");
+		scanner.close();
+		return scoreCheck();
+	}
+}
---
+++ gui/games/GinRummy.java
@@ -0,0 +1,241 @@
+package gui.games;
+
+import text.backend.Card;
+import text.backend.Deck;
+import text.backend.Game;
+import text.backend.Hand;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class GinRummy extends Game {
+	
+	public int cost() {
+		return 20;
+	}
+	
+	public String rules() {
+		return "In this 2 player game, players are trying to make matches with their cards. To win, a player must " +
+				       "have 2 unique matches of 3 different cards, and 1 separate match of 4 different cards. A " +
+				       "3-card match can either be a series match or a rank match, while a 4-card match can only be a" +
+				       " series match. A rank match is a match where all the cards have the same getRank() (e.g. the " +
+				       "7 of Spades, 7 of Diamonds, and 7 of Hearts are a rank match). A series match consists of 3 " +
+				       "or 4 cards that all have the same suit, and form a series with their rank. Aces can be a part" +
+				       " of a Queen-King series, or a 3-2 series (e.g. the Ace of Clubs, King, of Clubs, and Queen of" +
+				       " Clubs, the Jack of Hearts, 10 of Hearts, and 9 of Hearts, and the 4 of Spades, 3 of Spades, " +
+				       "2 of Spades, and the Ace of Spades, are all series matches, but the 2 of Hearts, Ace of " +
+				       "Hearts, and King of Hearts, and the 5 of Spades, 6 of Hearts, 7 of Diamonds, and 8 of Clubs, " +
+				       "are not).";
+	}
+	
+	public String toString() {
+		return "Gin Rummy (broken)";
+	}
+	
+	protected int playerLimit() {
+		return 2;
+	}
+	
+	/**
+	 * deck of cards
+	 */
+	private Deck stock;
+	/**
+	 * pile of cards already played
+	 */
+	private ArrayList<Card> centerPile;
+	
+	/**
+	 * one player turn
+	 *
+	 * @param hand hand of player
+	 * @param name name of player
+	 */
+	private void turn(Hand hand, String name) {
+		if (stock.size() == 0) {
+			Card temp = centerPile.get(centerPile.size() - 1);
+			stock.addAll(centerPile);
+			stock.shuffle();
+			centerPile.add(temp);
+			System.out.println("Reshuffled the center pile. It's now in the stock!");
+		}
+		Scanner scanner = new Scanner(System.in);
+		hand.printHand();
+		char pileType;
+		while (true) {
+			try {
+				System.out.println(
+						"\nWould you like to take a card from the stock or a card from the center pile (The " +
+								"current top card is " + centerPile.get(centerPile.size() - 1) + ")? Enter 'stock' to" +
+								" take from the stock or 'center' to take from the center pile?");
+				pileType = scanner.next().toLowerCase().charAt(0);
+			} catch (IndexOutOfBoundsException e) {
+				System.out.println("Here, take a card from the stock.");
+				pileType = 's';
+			}
+			if (pileType == 's' || centerPile.size() == 0) {
+				while (true) {
+					System.out.println("Here is your card: " + stock.get(0) + ". Would you like to take it or leave " +
+							                   "it?");
+					char take = scanner.next().toLowerCase().charAt(0);
+					if (take == 't') {
+						while (true) {
+							try {
+								System.out.println(
+										"Which card would you like to drop, " + name + "? Input it's corresponding " +
+												"value.");
+								int index = scanner.nextInt() - 1;
+								hand.add(stock.draw());
+								centerPile.add(hand.get(index));
+								hand.remove(index);
+								break;
+							} catch (InputMismatchException e) {
+								System.out.println("Please only input a number.");
+							} catch (IndexOutOfBoundsException e) {
+								System.out.println("That number isn't valid. Try again!");
+							}
+						}
+						hand.organizeCards();
+						break;
+					} else if (take == 'l') {
+						System.out.println("Alright, that card has been placed in the center.");
+						centerPile.add(stock.draw());
+						break;
+					}
+					System.out.println("Please only input 'take' and 'leave'.\n");
+				}
+				break;
+			} else if (pileType == 'c') {
+				while (true) {
+					try {
+						System.out.println("The card last put down is the " +
+								                   centerPile.get(centerPile.size() - 1) +
+								                   ". Which card will you substitute for it? Enter its index.");
+						int index = scanner.nextInt() - 1;
+						hand.add(stock.draw());
+						centerPile.add(hand.get(index));
+						hand.remove(index);
+						break;
+					} catch (InputMismatchException e) {
+						System.out.println("Please only input a number.");
+					} catch (IndexOutOfBoundsException e) {
+						System.out.println("That number isn't valid. Try again!");
+					}
+				}
+				hand.organizeCards();
+				break;
+			}
+			System.out.println("Whoops, that input didn't work, try again.");
+		}
+		scanner.close();
+	}
+	
+	/**
+	 * @param hand hand to check for a win
+	 * @return has the hand won
+	 */
+	private boolean checkForWin(Hand hand) {
+		int matches = 0;
+		for (Card card1 : hand) {
+			for (Card card2 : hand) {
+				for (Card card3 : hand) {
+					for (Card card4 : hand) {
+						if ((((card1.getRankValue() + 1 == card2.getRankValue() && card2.getRankValue() + 1 ==
+								                                                 card3.getRankValue() && card3.getRankValue() + 1 == card4.getRankValue()) ||
+								      (card1.getRankValue() == 14 && card2.getRankValue() == 2 && card3.getRankValue() == 3
+										       && card4.getRankValue() == 4)) && card1.getSuit().equals(card2.getSuit()) &&
+								     card3.getSuit().equals(card2.getSuit()) && card3.getSuit().equals(card4.getSuit())) &&
+								    (!card1.isCounted() && !card2.isCounted() && !card3.isCounted() && !card4.isCounted())) {
+							matches += 2;
+							card1.setCounted(true);
+							card2.setCounted(true);
+							card3.setCounted(true);
+							card4.setCounted(true);
+							System.out.println(
+									matches + "\nHere it is: " + card1 + ", and " + card2 + ", and" +
+											" " + card3 + ", and " + card4 + ".");
+						}
+					}
+				}
+			}
+		}
+		for (Card card1 : hand) {
+			for (Card card2 : hand) {
+				for (Card card3 : hand) {
+					if (((card1.getRankValue() + 1 == card2.getRankValue() && card2.getRankValue() + 1 == card3.getRankValue() &&
+							      card2.getSuit().equals(card3.getSuit()) && card2.getSuit().equals(card1.getSuit())) ||
+							     (card1.getRankValue() == 14 && card2.getRankValue() == 2 && card3.getRankValue() == 3)) && !(card1.isCounted()
+									                                                                                   || card2.isCounted() || card3.isCounted())) {
+						matches++;
+						card1.setCounted(true);
+						card2.setCounted(true);
+						card3.setCounted(true);
+						System.out.println(
+								matches + "\nHere it is: " + card1 + ", and " + card2 + ", and " + card3 + ".");
+					}
+				}
+			}
+		}
+		for (int i = 1; i < hand.size() - 1; i++) {
+			for (Card card1 : hand) {
+				for (Card card2 : hand) {
+					if ((hand.get(i).getRank().equals(card2.getRank()) && hand.get(i).getRank().equals(card1.getRank())) &&
+							    (!hand.get(i).getSuit().equals(card1.getSuit()) && !hand.get(i).getSuit().equals(card2.getSuit()))
+							    && !(hand.get(i).isCounted() || card1.isCounted() || card2.isCounted())) {
+						card1.setCounted(true);
+						hand.get(i).setCounted(true);
+						card2.setCounted(true);
+						matches++;
+						System.out.println(matches + "\nHere it is: " + card2 + ", and " + hand.get(i) + ", and " + card1 + ".");
+					}
+				}
+			}
+		}
+		for (Card card : hand) {
+			card.setCounted(false);
+		}
+		return matches == 4;
+	}
+	
+	public int play() throws InterruptedException {
+		centerPile = new ArrayList<>();
+		stock = new Deck();
+		stock.modifyRanks(new String[] {"Ace"}, new int[] {1});
+		int winner = 0;
+		boolean gameOver;
+		Hand handA = new Hand(10, stock, 1);
+		Hand handB = new Hand(10, stock, 1);
+		System.out.println("\n----------------------------\n\nLet's play Gin Rummy!\n");
+		Game.promptName(1, "Enter player 2's name: ");
+		while (winner == 0) {
+			System.out.println("\n\nNow it's " + Game.getName(0) + "'s turn. \nHere is your hand:\n");
+			turn(handA, Game.getName(0));
+			gameOver = checkForWin(handA);
+			if (gameOver) {
+				winner = 1;
+				break;
+			}
+			System.out.println("\n\nNow it's " + Game.getName(1) + "'s turn. \nHere is your hand:\n");
+			turn(handB, Game.getName(1));
+			gameOver = checkForWin(handB);
+			if (gameOver) {
+				winner = -1;
+			}
+		}
+		Thread.sleep(2000);
+		if (winner == 1) {
+			System.out.println("Congratulations " + Game.getName(0) + "! You beat " + Game.getName(1) + "!");
+		} else {
+			System.out.println("Congratulations " + Game.getName(1) + "! You beat " + Game.getName(0) + "!");
+		}
+		Thread.sleep(1000);
+		System.out.println("\n" + Game.getName(0) + "'s hand:");
+		handA.printHand();
+		Thread.sleep(1000);
+		System.out.println("\n" + Game.getName(1) + "'s hand:");
+		handB.printHand();
+		System.out.println("\n\nThanks for playing! Please come again soon!\n\n----------------------------\n\n");
+		return 1;
+	}
+}
