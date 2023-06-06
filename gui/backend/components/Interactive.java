6c3325c88df5331e5442566489b771fb1aaaae8d
Rit Garg <100240647+fielia@users.noreply.github.com>
2023-06-06 17:12:20 -0400

    creating text and user interaction gui component files

---
+++ gui/backend/components/Interactive.java
@@ -0,0 +1,7 @@
+package gui.backend.components;
+
+import javax.swing.JPanel;
+
+public class Interactive extends JPanel {
+	
+}
---
+++ gui/backend/components/TextArea.java
@@ -0,0 +1,7 @@
+package gui.backend.components;
+
+import javax.swing.JPanel;
+
+public class TextArea extends JPanel {
+	
+}
